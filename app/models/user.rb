d511696d2a5447621e1dea79d253df653c2f717d
pigats <andreapigato@gmail.com>
2017-01-05 16:58:39 +0100

    add User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :email, format: { with: /@{1}/, message: 'should be a valid email address' }
 end
