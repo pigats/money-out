8a0b9287c7a178d346030aa741ca1669e3ac7fc7
pigats <andreapigato@gmail.com>
2017-01-07 00:27:00 +0100

    add token based authentication to the API
    - use knock http://github.com/nsarno/knock (JWT), mounted on `/session`
    - add `/users/me` endpoint to retrieve current user
    - filter password digest in returned json
    - add gravatar based `avatar_url` to User
    - add hardcoded `user_manager` role to the API

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :email, uniqueness: true
   validates :email, format: { with: /@{1}/, message: 'should be a valid email address' }
+
+  def avatar_url
+    "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(self.email)}"
+  end
+
+  def is_user_manager
+    true
+  end
+
+  def as_json(options)
+    super(options.merge(except: 'password_digest', methods: :avatar_url))
+  end
 end
